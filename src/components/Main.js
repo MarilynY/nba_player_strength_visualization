b239bb6d3078bea4781effb7ef82c72b4eeabd6a
Menglin Yu <myu311@gatech.edu>
2019-09-17 04:36:02 -0400

    SearchBar almost done

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,13 +6,24 @@ import { SearchBar } from './SearchBar';
 
 export class Main extends React.Component {
     state = {
-        playerId: 201939,
-        playerInfo: []
+        playerInfo: {
+            playerName: 'Stephen Curry',
+            playerId: 201939
+        }
     }
 
     componentDidMount() {
+        this.loadPlayerInfo(this.state.playerInfo.playerName);
+    }
+
+    loadPlayerInfo = (playerName) => {
+        //Given player name
+        //0. get player id from player name
+        //1. fire api to get player info
+        //2. set state
+        const { playerId } = nba.findPlayer(playerName);
         nba.stats.playerInfo({
-            PlayerID: this.state.playerId 
+            PlayerID: playerId 
         }).then((info) => {
             console.log("player info log: ", info);
             // commonPlayerInfo: [{…}]
@@ -25,14 +36,15 @@ export class Main extends React.Component {
         }).catch((e) => {
             console.log(e);
         })
+
     }
     render() {
         return (
             <div className="main">
-                <SearchBar />
+                <SearchBar loadPlayerInfo={this.loadPlayerInfo}/>
                 <div className="player">
                     <Profile playerInfo={this.state.playerInfo}/>
-                    <DataViewContainer playerId={this.state.playerId}/>
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
                 </div>
             </div>
         );
