77f7b7f1fdc9251ea74cef4951d511351dc94369
Menglin Yu <myu311@gatech.edu>
2019-09-17 03:41:46 -0400

    SearchBar css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
 
+
+
+  
 export class SearchBar extends React.Component {
-    render () {
-        return (
-            <div>Search bar</div>
-        );
+    state = {
+      dataSource: [],
+    };
+  
+    handleSearch = (value) => {
+        console.log(value);
+        this.setState({
+            dataSource: !value ? [] : [value, value.repeat(2), value.repeat(3)],
+        });
+    };
+  
+    onSelect = (value) => {
+        console.log('onSelect', value);
     }
-}
+  
+    render() {
+      const { dataSource, value } = this.state;
+      return (
+        <div>
+            <AutoComplete
+                className="search-bar"
+                size="large"
+                dataSource={dataSource}
+                onSelect={this.onSelect}
+                onSearch={this.handleSearch}
+                placeholder="Search NBA Player"
+            >
+               <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
+
+        </div>
+      );
+    }
+  }
